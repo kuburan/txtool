e359e55af6db10dbe9eeea0f702e93ab645e73cd
kuburan <kuburan000@protonmail.ch>
2018-02-12 15:30:56 +0700

    delete unused dork

--- module/module15.py
+++ module/module15.py
@@ -9,7 +9,7 @@ import sub_menu2 as back
 
 
 def menu_utama():
-    print(warna.hijau + "\n\t[*]" + warna.tutup + warna.abuabu + "  Page 1 - Page 7 " + warna.tutup)
+    print(warna.hijau + "\n\t[*]" + warna.tutup + warna.abuabu + "  Page 1 - Page 8 " + warna.tutup)
     print(warna.hijau + "\t[*]" + warna.tutup + warna.abuabu + "  Press 0 to back\n" + warna.tutup)
     print(warna.kuning + "\n[!] " + warna.tutup + " These files contain usernames, but no passwords... Still, Google finding usernames on a web site. \n")
     info_page()
@@ -43,9 +43,9 @@ def menu1():
 
         else:
             filewrite = open(txtool_dir + "/dork.txt", "w")
-            filewrite.write("""\ninurl:/_layouts/mobile/view.aspx?List=\ninurl:/profile.php?lookup=1\n"authentication failure; logname=" ext:log\n""")
+            filewrite.write("""\ninurl:/_layouts/mobile/view.aspx?List=\ninurl:/profile.php?lookup=1 -git -stackoverflow\n""")
             filewrite.close()
-            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 -t 1 --sall %s" %
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
                 (inurl, txtool_dir, output), shell=True).wait()
             finish_dorking()
             print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
@@ -73,9 +73,9 @@ def menu2():
 
         else:
             filewrite = open(txtool_dir + "/dork.txt", "w")
-            filewrite.write("""\nintext:"root:x:0:0:root:/root:/bin/bash" inurl:*=/etc/passwd\ninurl:"/root/etc/passwd" intext:"home/*:"\nsite:extremetracking.com inurl:"login="\n""")
+            filewrite.write("""\n"authentication failure; logname=" ext:log -git\ninurl:"/root/etc/passwd" intext:"home/*:" -git -ibutterfly -questions -CodeWeblog.com -site:"https://nomads.in" -intext:"Forbidden"  -intitle:"Free convertor"\nsite:extremetracking.com inurl:"login=" -intitle:"/ Lost my Code / My Account"\n""")
             filewrite.close()
-            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 -t 1 --sall %s" %
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
                 (inurl, txtool_dir, output), shell=True).wait()
             finish_dorking()
             print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
@@ -103,9 +103,9 @@ def menu3():
 
         else:
             filewrite = open(txtool_dir + "/dork.txt", "w")
-            filewrite.write("""\nintext:"SteamUserPassphrase=" intext:"SteamAppUser=" -"username" -"user"\ninurl:root.asp?acs=anon\nfiletype:conf inurl:proftpd.conf -sample\n""")
+            filewrite.write("""\nfiletype:conf inurl:proftpd.conf -Fossies -Examples -Blog -sample -git -Searchcode -gitlab -Projects -Debian\nfiletype:reg reg HKEY_CURRENT_USER username\n""")
             filewrite.close()
-            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 -t 1 --sall %s" %
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
                 (inurl, txtool_dir, output), shell=True).wait()
             finish_dorking()
             print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
@@ -133,9 +133,9 @@ def menu4():
 
         else:
             filewrite = open(txtool_dir + "/dork.txt", "w")
-            filewrite.write("""\nfiletype:log username putty\nfiletype:reg reg +intext:"internet account manager"\nfiletype:reg reg HKEY_CURRENT_USER username\n""")
+            filewrite.write("""\nfiletype:log username putty -git\nfiletype:reg "reg" intext:"internet account manager"\n""")
             filewrite.close()
-            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 -t 1 --sall %s" %
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
                 (inurl, txtool_dir, output), shell=True).wait()
             finish_dorking()
             print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
@@ -163,9 +163,9 @@ def menu5():
 
         else:
             filewrite = open(txtool_dir + "/dork.txt", "w")
-            filewrite.write("""\n+intext:"webalizer" +intext:"Total Usernames" +intext:"Usage Statistics for"\ninurl:php inurl:hlstats intext:"Server Username"\nindex.of perform.ini\n""")
+            filewrite.write("""\nGoogle for: +intext:"webalizer" +intext:"Total Usernames" +intext:"Usage Statistics for"\ninurl:php inurl:hlstats intext:"Server Username"\n""")
             filewrite.close()
-            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 -t 1 --sall %s" %
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
                 (inurl, txtool_dir, output), shell=True).wait()
             finish_dorking()
             print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
@@ -193,9 +193,9 @@ def menu6():
 
         else:
             filewrite = open(txtool_dir + "/dork.txt", "w")
-            filewrite.write("""\n"index of" / lck\ninurl:admin filetype:asp inurl:userlist\ninurl:admin inurl:userlist\n""")
+            filewrite.write("""\n"index of" / lck -Google -Github -gitlab -git -forums -ghdb -books\ninurl:admin filetype:asp inurl:userlist\n""")
             filewrite.close()
-            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 -t 1 --sall %s" %
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
                 (inurl, txtool_dir, output), shell=True).wait()
             finish_dorking()
             print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
@@ -223,9 +223,39 @@ def menu7():
 
         else:
             filewrite = open(txtool_dir + "/dork.txt", "w")
-            filewrite.write("""\nintitle:index.of .sh_history\nintitle:index.of .bash_history\n""")
+            filewrite.write("""\nintitle:"Index of" .sh_history -git -Github -gitlab -Google -stackoverflow -site:"https://myechooff.wordpress.com"\nintitle:index.of .bash_history -git -Google -github -questions -stackoverflow\n""")
             filewrite.close()
-            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 -t 1 --sall %s" %
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
+                (inurl, txtool_dir, output), shell=True).wait()
+            finish_dorking()
+            print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
+                (output))
+            raw_input("\n press <" + warna.hijau + "Enter" + warna.tutup + "> to continue ")
+            os.system("cd %s && rm -fr dork.txt" % (txtool_dir))
+            back.menu['menu_utama']()
+
+    except(KeyboardInterrupt):
+        print(warna.merah + "\n[x] " + warna.tutup + "{0}CTRL+C{1} Detected, force program to stop !\n".format(warna.merah, warna.tutup))
+        os.system("cd %s && rm -fr dork.txt" % (txtool_dir))
+        print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s \n" %
+                (output))
+        sys.exit()
+
+
+def menu8():
+    try:
+        IP()
+        inurl = '/data/data/com.termux/files/usr/share/txtool/core'
+        output = raw_input(warna.biru + "\n[+] " + warna.tutup + " Input the output file you want " + warna.kuning + " >> " + warna.tutup)
+        if output == '':
+            empty()
+            back.menu['menu_utama']()
+
+        else:
+            filewrite = open(txtool_dir + "/dork.txt", "w")
+            filewrite.write("""\nintitle:"index of /" intext:"perform.ini" -Google -Github -git -books\n""")
+            filewrite.close()
+            subprocess.Popen("cd %s && ./inurlbr --no-banner --dork-file %s/dork.txt -q 1,6 --sall %s" %
                 (inurl, txtool_dir, output), shell=True).wait()
             finish_dorking()
             print(warna.hijau + "[*] " + warna.tutup + " Crawl result has been saved to /data/data/com.termux/files/home/.txtool/output/%s " %
@@ -255,5 +285,6 @@ menu = {
     '5': menu5,
     '6': menu6,
     '7': menu7,
+    '8': menu8,
     '0': Kembali,
 }
